b20b7b3102804ff6c56d442b3e6f46c6ef2a8182
YRMLunar <198003617@qq.com>
2025-07-30 20:55:04 +0800

    111

---
+++ tasks/organization/assets/assets_organization.py
@@ -0,0 +1,53 @@
+from module.base.button import Button, ButtonWrapper
+
+# This file was auto-generated, do not modify it manually. To generate:
+# ``` python -m dev_tools.button_extract ```
+
+MAIN_GOTO_ORGANIZATION = ButtonWrapper(
+    name='MAIN_GOTO_ORGANIZATION',
+    share=[
+        Button(
+            file='./assets/share/organization/MAIN_GOTO_ORGANIZATION.png',
+            area=(685, 292, 724, 362),
+            search=(665, 272, 744, 382),
+            color=(115, 124, 110),
+            button=(685, 292, 724, 362),
+        ),
+        Button(
+            file='./assets/share/organization/MAIN_GOTO_ORGANIZATION.2.png',
+            area=(498, 282, 544, 373),
+            search=(478, 262, 564, 393),
+            color=(92, 110, 89),
+            button=(498, 282, 544, 373),
+        ),
+    ],
+)
+ORGANIZATION_PANEL = ButtonWrapper(
+    name='ORGANIZATION_PANEL',
+    share=Button(
+        file='./assets/share/organization/ORGANIZATION_PANEL.png',
+        area=(153, 42, 384, 151),
+        search=(133, 22, 404, 171),
+        color=(163, 96, 35),
+        button=(153, 42, 384, 151),
+    ),
+)
+ORGANIZATION_RED_DOT = ButtonWrapper(
+    name='ORGANIZATION_RED_DOT',
+    share=[
+        Button(
+            file='./assets/share/organization/ORGANIZATION_RED_DOT.png',
+            area=(688, 270, 734, 341),
+            search=(668, 250, 754, 361),
+            color=(120, 131, 95),
+            button=(688, 270, 734, 341),
+        ),
+        Button(
+            file='./assets/share/organization/ORGANIZATION_RED_DOT.2.png',
+            area=(505, 264, 559, 365),
+            search=(485, 244, 579, 385),
+            color=(112, 132, 89),
+            button=(505, 264, 559, 365),
+        ),
+    ],
+)
